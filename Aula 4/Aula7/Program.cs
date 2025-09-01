1c8f6f9efecdb77d64f9998e4b74fb4e61299f6e
erick.paludo <erick@fullsoft.com.br>
2025-09-01 14:00:23 -0300

    Exercicio 7 resolvido

--- Aula 4/Aula7/Program.cs
+++ Aula 4/Aula7/Program.cs
@@ -10,12 +10,25 @@
             //int[] vetor = { 1, 2, 2, 3, 4, 4, 5 };
             //Exemplo de Saída:
             //Vetor sem Duplicados: { 1, 2, 3, 4, 5 }
-            int[] vetor = { 1, 2, 2, 3, 4, 4, 5 };
+            int[] vetor = { 1, 2, 2, 3, 4, 4, 5,8,8,9,6,4,8,1,1,6,4,5,5,4,5 };
+           
+            foreach(var obj in vetor)
+            {
+                Console.Write($"{obj} ");
+            }
+            Console.WriteLine();
+            foreach(var obj in RemoverDuplicados(vetor))
+            {
+                Console.Write($"{obj} ");
+            }
+        }    
+        static int[] RemoverDuplicados(int[] vetor)
+        {
             int[] repitido = new int[vetor.Length];
 
             int indicerepitido = 0;
-            bool existe = false;
             int cont = 0;
+            int tamanho = 1;
             repitido[indicerepitido] = vetor[0];
 
             for (int i = 0; i < vetor.Length; i++)
@@ -29,13 +42,19 @@
                 }
                 if (cont < 1)
                 {
+                    tamanho++;
                     indicerepitido++;
                     repitido[indicerepitido] = vetor[i];
                 }
-                    cont = 0;
+                cont = 0;
             }
-            Console.WriteLine(repitido);
-        }    
+            int[] semduplicatas = new int[tamanho];
+            for (int i = 0; i < semduplicatas.Length; i++)
+            {
+                semduplicatas[i] = repitido[i];
+            }
+            return semduplicatas;
+        }
     }
 }
 
